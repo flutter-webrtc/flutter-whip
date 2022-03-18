43b87d7db679d5512467ec079232d76d4e0605e5
cloudwebrtc <duanweiwei1982@gmail.com>
2022-03-18 08:56:09 +0800

    chore: add full example.

---
+++ example/android/app/src/main/java/com/cloudwebrtc/flutterwhip/example/flutter_whip_example_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.cloudwebrtc.flutterwhip.example.flutter_whip_example_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
